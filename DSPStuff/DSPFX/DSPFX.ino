f4f54d762a0fa6bc116d891b3d8736bc205cb9a2
Thompson Truong <thomtruong545@utexas.edu>
2025-11-29 18:02:10 -0600

    added arduino code

---
+++ DSPStuff/DSPFX/DSPFX.ino
@@ -0,0 +1,66 @@
+uint32_t inputdata[10];
+bool flangerFlag;
+bool pauseFlag;
+bool distortionFlag;
+bool reverbFlag;
+bool delayFlag;
+bool tempoFlag;
+
+#define INPUT_SCALE_FACTOR 0.0003125
+#define OUTPUT_SCALE_FACTOR 3200.0
+
+void setup() {
+  // put your setup code here, to run once:
+  ESPUARTsetup();
+  for (int i = 0; i < 10; i++) {
+    inputdata[i] = 0;
+  }
+  flangerFlag = false;
+  pauseFlag = false;
+  distortionFlag = false;
+  reverbFlag = false;
+  delayFlag = false;
+  tempoFlag = false;
+}
+void loop() { // assuming left and right samples sent
+  //uint16_t outputsampledata = inputsampledata;
+  UARTRecieve(&inputdata[0]);
+  if (inputdata[0] & 0x1) { // Pause Button
+    pauseFlag = !pauseFlag;
+  }
+  if (inputdata[0] & 0x4) { // Flanger Button
+    flangerFlag = !flangerFlag;
+  }
+  if (inputdata[0] & 0x8) { // Distortion Button
+    distortionFlag = !distortionFlag;
+  }
+  if (inputdata[0] & 0x10) { // Reverb Button
+    reverbFlag = !reverbFlag;
+  }
+  if (inputdata[0] & 0x20) { // Tempo Button
+    tempoFlag = !tempoFlag;
+  }
+  if (inputdata[0] & 0x80) { // Delay Button
+    delayFlag = !delayFlag;
+  }
+
+  if (flangerFlag) {
+    //outputsampledata = flanger_process_left_sample(outputsampledata);
+  }
+  if (tempoFlag) {
+
+  }
+  if (pauseFlag) {
+
+  }
+  if (delayFlag) {
+
+  }
+  if (reverbFlag) {
+    
+  }
+  if (distortionFlag) {
+    //outputsampledata = distortion_process_left_sample(outputsampledata); // if we have stereo, have a left and right
+  }
+  //return outputsampledata;
+}
