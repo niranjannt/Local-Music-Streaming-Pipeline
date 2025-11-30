ec5322384b32ef37399f74c6f63f3884b14000b0
thomtruong545 <thomtruong545@utexas.edu>
2025-11-30 14:07:36 -0600

    Update TM4C2ESP.ino

--- DSPStuff/DSPFX/TM4C2ESP.ino
+++ DSPStuff/DSPFX/TM4C2ESP.ino
@@ -4,23 +4,23 @@ uint32_t HEADER;
 void ESPUARTsetup() {
   HEADER = 0xFFFF;
   foundHEADER = false;
-  Serial.begin(9600);
+  Serial1.begin(0, SERIAL_8N1, -1, -1, false, 20000);
 }
 
 void UARTRecieve(uint32_t* data) {
   uint32_t header2compare = 0;
   if (foundHEADER) {
-    while (Serial.available() < 20) {}
+    while (Serial1.available() < 20) {}
     for (int i = 0; i < 10; i++) {
-      data[i] = Serial.read();
-      data[i] = data[i] + (Serial.read() << 8);
+      data[i] = Serial1.read();
+      data[i] = data[i] + (Serial1.read() << 8);
     }
     foundHEADER = false;
   }
   else { // finding header
-    while (Serial.available() < 2) {}
-    header2compare = Serial.read();
-    header2compare = header2compare + (Serial.read() << 8);
+    while (Serial1.available() < 2) {}
+    header2compare = Serial1.read();
+    header2compare = header2compare + (Serial1.read() << 8);
     if (header2compare == HEADER) {
       foundHEADER = true;
     }
