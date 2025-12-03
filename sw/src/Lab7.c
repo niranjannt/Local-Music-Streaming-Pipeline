f004c0ed813575026e915e0e325be8834e85bd0c
Ashton <ashtonknecht@gmail.com>
2025-12-03 13:52:19 -0600

    Fixed analog volume control - 1 and 2 are still switched

--- sw/src/Lab7.c
+++ sw/src/Lab7.c
@@ -15,7 +15,7 @@
 // *****LCD graphics, and I/O with SDC***************
 // Backlight (pin 10) connected to +3.3 V
 // MISO (pin 9) connected to PA4 (SSIRx for SDC)
-// SCK (pin 8) connected to PA2 (SSI0Clk)
+/+/ SCK (pin 8) connected to PA2 (SSI0Clk)
 // MOSI (pin 7) connected to PA5 (SSI0Tx)
 // TFT_CS (pin 6) connected to PA3 (SSI0Fss)
 // CARD_CS (pin 5) connected to PD7 (CS for SDC)
@@ -41,19 +41,32 @@
 #include "../inc/Timer2A.h"
 #include "../inc/TLV5616.h"
 #include "AudioCommands.h"
+#include "Volume.h"
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 void WaitForInterrupt(void);  // low power mode
 
-
+_Bool changeVolumes = 0;
+unsigned bassVol = 0;
+unsigned midVol = 0;
+unsigned trebVol = 0;
+unsigned mainVol = 0;
 
 int main(void){
   DisableInterrupts();
   PLL_Init(Bus80MHz);    // bus clock at 80 MHz
   AudioCommandInit(); // initializes UART, Switches and Potentiometer
+  initVolume();
   EnableInterrupts();
   while(1){
-      UpdateLCD();
+      //UpdateLCD();
+      if (changeVolumes) {
+          setBass(bassVol);
+          setMid(midVol);
+          setTreb(trebVol);
+          setVolume(mainVol);
+          changeVolumes = 0;
+      }
    }
 }
    
