b5d686a2c893fb8e27b175ebd0471b7f02c0afd2
thomtruong545 <thomtruong545@utexas.edu>
2025-12-02 20:48:28 -0600

    Update AudioCommands.c test code

--- sw/src/AudioCommands.c
+++ sw/src/AudioCommands.c
@@ -35,7 +35,7 @@ void AudioCommandInit() {
     GPIO_PORTB_AFSEL_R |= 0x03;           // enable alt funct on PB1-0
     GPIO_PORTB_DEN_R |= 0x03;             // enable digital I/O on PB1-0
                                           // configure PA1-0 as UART
-    GPIO_PORTB_PCTL_R = (GPIO_PORTA_PCTL_R&0xFFFFFF00)+0x00000011;
+    GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R&0xFFFFFF00)+0x00000011;
     GPIO_PORTB_AMSEL_R &= ~0x03;          // disable analog functionality on PB
 
     PotInit();
@@ -49,8 +49,20 @@ void AudioCommandInit() {
 
 
 void AudioCommandsHandler() {
-    data[9] = SwitchIn();
-    PotIn(&data[0]);
+    //data[9] = SwitchIn();
+    //PotIn(&data[0]);
+    // Test Code
+    data[9] = 800;
+    data[8] = 12;
+    data[7] = 4095;
+    data[6] = 404;
+    data[5] = 0;
+    data[4] = 2400;
+    data[3] = 1206;
+    data[2] = 1000;
+    data[1] = 505;
+    data[0] = 7;
+    //
     AudioCommandOut();
 }
 
@@ -58,7 +70,7 @@ void UART_Out(uint32_t n){
     uint32_t transmit = n & 0xFF; // Lower 8 bits of data
     while((UART1_FR_R&UART_FR_TXFF) != 0);
     UART1_DR_R = transmit;
-    transmit = n >> 8; // Upper 8 bits of data if needed
+    transmit = (n & 0xF00) >> 8; // Upper 4 bits of data if needed
     while((UART1_FR_R&UART_FR_TXFF) != 0);
     UART1_DR_R = transmit;
 }
